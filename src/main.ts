c475f34293b19b5cf2049a4b795d9b1d3116411c
AI-Spawn <50590465+AI-Spawn@users.noreply.github.com>
2021-04-22 18:42:47 -0400

    working demo

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+let x: number = 500;
+
+let speed: number = 500;
+
+let cnv: p5.Renderer;
+let moveUpdate: number = Date.now();
+function setup() {
+  cnv = createCanvas(windowWidth, windowHeight);
+  cnv.position(0, 0);
+}
+function draw() {
+  background(0);
+
+  ellipse(x, height / 2, 50, 50);
+
+  x += ((Date.now() - moveUpdate) / 1000) * speed;
+
+  if (x < 25) {
+    speed *= -1;
+    x = 25;
+  }
+  if (x > width - 25) {
+    speed *= -1;
+    x = width - 25;
+  }
+  moveUpdate = Date.now();
+}
